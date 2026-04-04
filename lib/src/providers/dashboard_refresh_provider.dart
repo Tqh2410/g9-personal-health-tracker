f1021f6744a940199a95d37d35ea8430e5070e20
TuyenTran205 <tuyenthe431@gmail.com>
2026-04-04 21:03:05 +0700

    Revert "feat(home): add home drawer and dashboard sync flows"
    This reverts commit 41e798b16d69de420422c8fbcb7123529ef25eee.

--- lib/src/providers/dashboard_refresh_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final dashboardRefreshTriggerProvider = StateProvider<int>((ref) => 0);
-
-void triggerDashboardRefresh(WidgetRef ref) {
-  ref.read(dashboardRefreshTriggerProvider.notifier).state++;
-}
